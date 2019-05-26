ae2ae7ecddaf6c691e7ccf57b62221883f90b8b0
MrNaif2018 <chuff184@gmail.com>
2019-05-26 19:40:48 +0300

    Fix some bugs, add new methods for polling updates

--- bitcart/coins/btc.py
+++ bitcart/coins/btc.py
@@ -1,30 +1,35 @@
 # pylint: disable=import-error, invalid-sequence-index
 import sys
+import time
+import logging
 sys.path.append("..")
 try:
     from ..coin import Coin
-except (ValueError,ImportError):
+except (ValueError, ImportError):
     from coin import Coin
 
-class BTC(Coin):
-    coin_name="BTC"
-    friendly_name="Bitcoin"
-    providers=["jsonrpcrequests"]
 
-    def __init__(self,rpc_url,rpc_user=None,rpc_pass=None,xpub=None):
+class BTC(Coin):
+    coin_name = "BTC"
+    friendly_name = "Bitcoin"
+    providers = ["jsonrpcrequests"]
+
+    def __init__(self, rpc_url, rpc_user=None, rpc_pass=None, xpub=None):
         super().__init__()
         self.rpc_url = rpc_url
         self.rpc_user = rpc_user
         self.rpc_pass = rpc_pass
-        self.xpub=xpub
-        self.server = self.providers["jsonrpcrequests"].RPCProxy(self.rpc_url,self.rpc_user,self.rpc_pass,self.xpub)
+        self.xpub = xpub
+        self.notify_func = None
+        self.server = self.providers["jsonrpcrequests"].RPCProxy(
+            self.rpc_url, self.rpc_user, self.rpc_pass, self.xpub)
 
     def get_tx(self, tx: str) -> dict:
-        out=self.server.get_transaction(tx)
+        out = self.server.get_transaction(tx)
         try:
-            out["input"]=out["inputs"][0]["address"]
-        except (KeyError,IndexError):
-            out["input"]=None
+            out["input"] = out["inputs"][0]["address"]
+        except (KeyError, IndexError):
+            out["input"] = None
         return out
 
     def get_address(self, address: str) -> list:
@@ -36,14 +41,33 @@ class BTC(Coin):
     def balance(self) -> dict:
         data = self.server.getbalance()
         return {"confirmed": data.get("confirmed"),
-            "unconfirmed": data.get("unconfirmed"),
-            "unmatured": data.get("unmatured")}
+                "unconfirmed": data.get("unconfirmed"),
+                "unmatured": data.get("unmatured")}
 
     def addrequest(self, amount, description="", expire=15) -> dict:
         return self.server.addrequest(amount=amount, memo=description, expiration=60*expire, force=True)
-        
+
     def getrequest(self, address: str) -> dict:
         return self.server.getrequest(address)
 
     def history(self) -> list:
         return self.server.history()
+
+    def notify(self, f):
+        self.notify_func = f
+        return f
+
+    def poll_updates(self, timeout=2):
+        if not self.notify_func:
+            raise AttributeError(
+                "No notification function set. Set it with @notify decorator")
+        while True:
+            try:
+                data = self.server.notify_tx()
+            except Exception as err:
+                logging.error(err)
+                time.sleep(timeout)
+                continue
+            if data:
+                self.notify_func(data)
+            time.sleep(timeout)
--- bitcart/providers/jsonrpcrequests.py
+++ bitcart/providers/jsonrpcrequests.py
@@ -2,55 +2,60 @@
 '''Author: Alexey Drozdov(MrNaif)
 Company name: Naif Studios
 Email: chuff184@gmail.com'''
-__author__="Alexey Drozdov(MrNaif)"
-__company__="Naif Studios"
-__email__="chuff184@gmail.com"
-__copyright__=True
+__author__ = "Alexey Drozdov(MrNaif)"
+__company__ = "Naif Studios"
+__email__ = "chuff184@gmail.com"
+__copyright__ = True
 try:
     import requests
 except ImportError:
     raise ImportError("You must install requests library first!")
 try:
     from simplejson import loads as json_loads
-except (ImportError,ValueError):
+except (ImportError, ValueError):
     from json import loads as json_loads
 import warnings
 
-class RPCProxy:
-    def __init__(self,url,username=None,password=None,xpub=None,verify=True):
-        self.url=url
-        self.username=username
-        self.password=password
-        self.xpub=xpub
-        self.verify=verify
 
-    def _send_request(self,method,*args,**kwargs):
+class RPCProxy:
+    def __init__(self, url, username=None, password=None, xpub=None, verify=True):
+        self.url = url
+        self.username = username
+        self.password = password
+        self.xpub = xpub
+        self.verify = verify
+
+    def _send_request(self, method, *args, **kwargs):
         if not self.username or not self.password:
-            auth=None
+            auth = None
         else:
-            auth=(self.username,self.password)
+            auth = (self.username, self.password)
         if args:
-            arg=args
+            arg = args
         elif kwargs:
-            arg=kwargs
+            arg = kwargs
         else:
-            arg=[]
-        dict_to_send={"id": 0,"method":method,"params":arg}
+            arg = []
+        dict_to_send = {"id": 0, "method": method, "params": arg}
         if self.xpub:
-            dict_to_send["xpub"]=self.xpub
-        response=requests.post(self.url,headers={'content-type': 'application/json'},json=dict_to_send,auth=auth,verify=self.verify)
+            dict_to_send["xpub"] = self.xpub
+        response = requests.post(self.url, headers={
+                                 'content-type': 'application/json'}, json=dict_to_send, auth=auth, verify=self.verify)
         response.raise_for_status()
-        json=response.json()
+        json = response.json()
         if json["error"]:
             raise ValueError("Error from server: {}".format(json["error"]))
         if json["id"] != 0:
             warnings.warn("ID mismatch!")
-        result=json.get("result",{})
+        result = json.get("result", {})
         if type(result) == str:
-            result=json_loads(result)
+            try:
+                result = json_loads(result)
+            except Exception:
+                pass
         return result
 
-    def __getattr__(self,method,*args,**kwargs):
-        def wrapper(*args,**kwargs):
-            return self._send_request(method,*args,**kwargs)
+    def __getattr__(self, method, *args, **kwargs):
+        def wrapper(*args, **kwargs):
+            return self._send_request(method, *args, **kwargs)
         return wrapper
