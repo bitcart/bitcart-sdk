59bf41e926e75c11fd1afa05ee0dad9602718163
MrNaif2018 <chuff184@gmail.com>
2019-08-08 13:26:07 +0300

    Version 0.2.6 async: fix __del__ method in uvloop and change warning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='bitcart-async',
     packages=find_packages(),
-    version='0.2.5',
+    version='0.2.6',
     license='MIT',
     description='Bitcart coins support library',
     long_description=open("README.md").read(),
