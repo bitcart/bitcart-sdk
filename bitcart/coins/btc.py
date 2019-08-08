7ac684f0c64b8023279e1993f034dcd0569ce2ec
MrNaif2018 <chuff184@gmail.com>
2019-08-08 13:27:40 +0300

    Version 0.2.5: fix warning

--- bitcart/coins/btc.py
+++ bitcart/coins/btc.py
@@ -30,8 +30,7 @@ class BTC(Coin):
         super().__init__()
         if not xpub:
             warnings.warn(
-                "Xpub not provided. Not all functions will be available.",
-                stacklevel=2)
+                "Xpub not provided. Not all functions will be available.")
         self.rpc_url = rpc_url or self.RPC_URL
         self.rpc_user = rpc_user or self.RPC_USER
         self.rpc_pass = rpc_pass or self.RPC_PASS
