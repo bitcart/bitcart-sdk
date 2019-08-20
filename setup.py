579d052dc933ad178ff1315b6fad56389e8fa227
MrNaif2018 <chuff184@gmail.com>
2019-08-20 16:08:14 +0300

    Bitcart Asyncio 0.3.0: events api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='bitcart-async',
     packages=find_packages(),
-    version='0.2.7',
+    version='0.3.0',
     license='MIT',
     description='Bitcart coins support library',
     long_description=open("README.md").read(),
