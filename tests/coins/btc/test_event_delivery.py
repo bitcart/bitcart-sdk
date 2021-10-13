ecad6bfd9cf350c04db4c8238f419457c9509209
MrNaif2018 <chuff184@gmail.com>
2021-10-14 00:56:34 +0300

    Use event loop properly

--- tests/coins/btc/test_event_delivery.py
+++ tests/coins/btc/test_event_delivery.py
@@ -3,6 +3,7 @@ import multiprocessing
 import pytest
 
 from bitcart.errors import ConnectionFailedError
+from tests.utils import patch_session
 
 test_queue = multiprocessing.Queue()
 
@@ -15,7 +16,7 @@ def new_tx_handler(event, tx):
 
 async def test_event_delivery(patched_session, btc_wallet, mocker):
     btc_wallet.add_event_handler("new_transaction", new_tx_handler)
-    mocker.patch.object(btc_wallet.server, "session", patched_session)
+    patch_session(mocker, patched_session)
     await btc_wallet.start_websocket(auto_reconnect=False)
     assert test_queue.qsize() == 1
     assert test_queue.get() is True
@@ -29,14 +30,14 @@ async def test_bad_connection(btc_wallet):
 
 async def test_bad_json(patched_session_bad_json, btc_wallet, mocker):
     btc_wallet.add_event_handler("new_transaction", new_tx_handler)
-    mocker.patch.object(btc_wallet.server, "session", patched_session_bad_json)
+    patch_session(mocker, patched_session_bad_json)
     await btc_wallet.start_websocket(auto_reconnect=False)
     assert test_queue.qsize() == 0
 
 
 async def test_bad_json_reconnect_callback(patched_session_bad_json, btc_wallet, mocker):
     btc_wallet.add_event_handler("new_transaction", new_tx_handler)
-    mocker.patch.object(btc_wallet.server, "session", patched_session_bad_json)
+    patch_session(mocker, patched_session_bad_json)
     await btc_wallet.start_websocket(auto_reconnect=False, reconnect_callback=lambda: test_queue.put(True))
     assert test_queue.qsize() == 1  # from reconnect_callback
     assert test_queue.get() is True
