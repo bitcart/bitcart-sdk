8f86e7571f1bb4da791af1b9633306b67c30f299
MrNaif2018 <chuff184@gmail.com>
2022-03-15 23:41:44 +0300

    Add types

--- bitcart/coins/eth.py
+++ bitcart/coins/eth.py
@@ -1,3 +1,5 @@
+from typing import Any, NoReturn
+
 from ..errors import NotImplementedError
 from .btc import BTC
 
@@ -9,10 +11,10 @@ class ETH(BTC):
     ALLOWED_EVENTS = ["new_block", "new_transaction", "new_payment"]
 
     async def history(self) -> dict:  # pragma: no cover
-        return await self.server.history()
+        return await self.server.history()  # type: ignore
 
-    async def get_address(self, *args, **kwargs):
+    async def get_address(self, *args: Any, **kwargs: Any) -> NoReturn:
         raise NotImplementedError("Full address history lookup not implemented for ETH to remain lightweight")
 
-    async def pay_to_many(self, *args, **kwargs):
+    async def pay_to_many(self, *args: Any, **kwargs: Any) -> NoReturn:
         raise NotImplementedError("Pay to many not available in ETH directly")
