923f87dfe157516635fa3180fd355e0cf9d9c0fe
MrNaif2018 <chuff184@gmail.com>
2022-03-15 23:33:36 +0300

    Add ETH

--- bitcart/coins/__init__.py
+++ bitcart/coins/__init__.py
@@ -1,9 +1,10 @@
 from .bch import BCH
 from .bsty import BSTY
 from .btc import BTC
+from .eth import ETH
 from .ltc import LTC
 from .xrg import XRG
 
-COINS = {"BTC": BTC, "BCH": BCH, "LTC": LTC, "BSTY": BSTY, "XRG": XRG}
+COINS = {"BTC": BTC, "BCH": BCH, "ETH": ETH, "LTC": LTC, "BSTY": BSTY, "XRG": XRG}
 
 __all__ = list(COINS.keys()) + ["COINS"]
