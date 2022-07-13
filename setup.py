b7cf46f53bdef8aae1ed44c4ba400520644b46a9
MrNaif2018 <chuff184@gmail.com>
2022-07-13 18:58:21 +0300

    Temporarily use electrum-upgrade branch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="bitcart",
     packages=find_packages(),
-    version="1.9.3.0",
+    version="1.10.0.0",
     license="LGPLv3+",
     description="BitcartCC coins support library",
     long_description=open("README.md").read(),
